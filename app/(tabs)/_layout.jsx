6e722fb792772fedc32a2141b02a97885d0ef735
Chris Williams <cchriswilliams721@gmail.com>
2024-12-06 14:30:38 -0700

    Added Nav Bar

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,37 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#3b3b3b' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="calender"
+        options={{
+          title: 'Calender',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="habittracker"
+        options={{
+          title: 'habit',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="check" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/habittracker.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Habittracker = () => {
+  return (
+    <View>
+      <Text>Habittracker</Text>
+    </View>
+  )
+}
+
+export default Habittracker
